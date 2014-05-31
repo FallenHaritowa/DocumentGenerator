35a16171d622f6483a3fcc5d6ba362ebd04ec280
Denis Haritonov <denisiccupgame@gmail.com>
2014-05-31 15:48:17 +0300

    Base Classes

---
+++ Document Generator/PatternLogic/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternLogic
+{
+    public static class Constants
+    {
+        public Dictionary<String, Type> ExentsiontSeparators;
+        public Constants()
+        {
+        }
+    }
+    
+}
---
+++ Document Generator/PatternLogic/File Classes/DocTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternLogic
+{
+    public class DocConfig
+    {
+        public List<Entity> EnabledEntities; 
+    }
+}
